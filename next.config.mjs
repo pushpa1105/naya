0179676974363393e218c2928d8c8931a1caf3ad
pushpa1105 <projectinfo1105@gmail.com>
2024-09-04 20:23:36 +0545

    feat(Error monitoring setup using Sentry & Spotlight): error monitoring with sentry & spotlight
    n

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 import withNextIntl from "next-intl/plugin";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import createJiti from "jiti";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti("./src/libs/env");
@@ -31,7 +32,7 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/:locale((?!en).*)", // #TODO: For supports english only
+        source: "/:locale((?!en).*)", // #TODO: For now supports english only
         destination: "/en",
         permanent: true,
       },
@@ -39,4 +40,42 @@ const nextConfig = {
   },
 };
 
-export default bundleAnalyzer(withNextIntlConfig(nextConfig));
+export default withSentryConfig(
+  bundleAnalyzer(withNextIntlConfig(nextConfig)),
+  {
+    org: "naya-0b",
+    project: "naya-dev",
+
+    // An auth token is required for uploading source maps.
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+
+    silent: false, // Can be used to suppress logs
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+
+    // Disable Sentry telemetry
+    telemetry: false,
+  },
+);
